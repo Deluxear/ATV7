7ba998a25976b11ae4f599e1b9ed71d875346fb0
Deluxear <89830002+Deluxear@users.noreply.github.com>
2021-11-06 00:01:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const Ano = 2021
+let NomeDaPessoa = prompt("Qual o seu nome?")
+let IdadeI = prompt ("Em qual ano você nasceu?") 
+let IdadeF = Ano - IdadeI;
+let mensagem = `Olá ${NomeDaPessoa}, nossa que legal você já tem ${IdadeF} anos passa o zap?`;
+
+alert (mensagem);
